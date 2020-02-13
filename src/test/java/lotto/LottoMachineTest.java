52f008ad29cf8f3915484d1f39a025e958b27992
최준우 <junwoochoi@zuminternet.com>
2020-02-13 10:00:17 +0900

    test: create LottoMachine test

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DisplayName("로또 기계 테스트")
+class LottoMachineTest {
+
+    @Test
+    @DisplayName("로또 객체를 반환한다.")
+    void createLottoNumbers() {
+        //given
+        LottoMachine lottoMachine = new LottoMachine();
+
+        //when
+        Lotto lotto = lottoMachine.createLotto();
+
+        //then
+        assertThat(lotto).isNotNull();
+    }
+}
