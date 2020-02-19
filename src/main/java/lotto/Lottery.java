753c983304be544e26b925d4a48f47e0ac08ee85
최준우 <junwoochoi@zuminternet.com>
2020-02-20 08:52:23 +0900

    refactor: change method order

--- src/main/java/lotto/Lottery.java
+++ src/main/java/lotto/Lottery.java
@@ -19,16 +19,24 @@ public class Lottery {
         this.numbers = lottoNumbers;
     }
 
-    private void validateNumbers(Set<Integer> lottoNumbers) {
-        if (lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException("로또의 갯수가 잘못되었거나 중복된 숫자가 있습니다.");
-        }
-    }
-
     public static Lottery of(List<Integer> lottoNumbers) {
         final HashSet<Integer> distinctNumbers = new HashSet<>(lottoNumbers);
 
         return new Lottery(distinctNumbers);
+    }
+
+    public Set<Integer> getNumbers() {
+        return new HashSet<>(numbers);
+    }
+
+    public Result checkResult(WinningNumbers winningNumbers) {
+        return Result.of(correctCount(winningNumbers));
+    }
+
+    private void validateNumbers(Set<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("로또의 갯수가 잘못되었거나 중복된 숫자가 있습니다.");
+        }
     }
 
     private int correctCount(WinningNumbers winningNumbers) {
@@ -37,14 +45,6 @@ public class Lottery {
         final int combinedSize = myNumbers.size();
 
         return LOTTO_NUMBER_COUNT + (LOTTO_NUMBER_COUNT - combinedSize);
-    }
-
-    public Result checkResult(WinningNumbers winningNumbers) {
-        return Result.of(correctCount(winningNumbers));
-    }
-
-    public Set<Integer> getNumbers() {
-        return new HashSet<>(numbers);
     }
 
 }
