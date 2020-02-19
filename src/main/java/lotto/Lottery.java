ea5cd7fc5eb12ba930108b98ed618db135db5f31
최준우 <junwoochoi@zuminternet.com>
2020-02-20 08:49:46 +0900

    refactor: gather validate function in constructor

--- src/main/java/lotto/Lottery.java
+++ src/main/java/lotto/Lottery.java
@@ -21,15 +21,12 @@ public class Lottery {
 
     private void validateNumbers(Set<Integer> lottoNumbers) {
         if (lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException("로또의 갯수가 잘못되었습니다.");
+            throw new IllegalArgumentException("로또의 갯수가 잘못되었거나 중복된 숫자가 있습니다.");
         }
     }
 
     public static Lottery of(List<Integer> lottoNumbers) {
         final HashSet<Integer> distinctNumbers = new HashSet<>(lottoNumbers);
-        if (distinctNumbers.size() != lottoNumbers.size()) {
-            throw new IllegalArgumentException("로또에 중복된 숫자가 들어올 수 없습니다.");
-        }
 
         return new Lottery(distinctNumbers);
     }
