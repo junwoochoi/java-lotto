c01270c5c4fcbdc0f2ef8d14f84f2ba57de0cc58
junwoochoi <cjw4690@gmail.com>
2020-02-24 21:42:15 +0900

    test: test new features

---
+++ src/test/java/lotto/PrizeTest.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PrizeTest {
+
+    //given
+    @CsvSource({"6,false,FIRST_PRIZE", "5,true,SECOND_PRIZE", "5,false,THIRD_PRIZE", "4,false,FOURTH_PRIZE", "3,false,FIFTH_PRIZE", "1,false,NONE"})
+    @ParameterizedTest
+    @DisplayName("Prize 생성 테스트")
+    void testCreate(int matchCount, boolean isBonusMatch, Prize expected) {
+
+        //when
+        final Prize prize = Prize.ofMatchCount(matchCount, isBonusMatch);
+
+        //then
+        assertThat(prize).isEqualTo(expected);
+    }
+
+}
--- src/test/java/lotto/ResultTest.java
+++ src/test/java/lotto/ResultTest.java
@@ -14,7 +14,7 @@ class ResultTest {
         final Result result = Result.of(5, true);
 
         assertThat(result).isNotNull();
-        assertThat(result.getWonMoney()).isEqualTo(Prize.ofMatchCount(5, true).getRewardMoney());
+        assertThat(result.getPrize().getRewardMoney()).isEqualTo(Prize.ofMatchCount(5, true).getRewardMoney());
     }
 
 }
